cdd63d4f431a0216c8ab7938837d77002c047a74
shinusmp <shrinivaspatil12@gmail.com>
2022-06-12 22:23:53 +0530

    PHN-3: Create Required pckg Structure

---
+++ src/main/java/com/BikkadIt/phoneBook/Model/Contact.java
@@ -0,0 +1,7 @@
+package com.BikkadIt.phoneBook.Model;
+
+public class Contact {
+	
+	
+
+}
---
+++ src/test/java/com/BikkadIt/phoneBook/PhoneBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.phoneBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
