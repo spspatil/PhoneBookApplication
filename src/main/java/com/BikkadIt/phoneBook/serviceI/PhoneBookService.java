582ef73e50a025b04129cfa7e5ad59d38b1b0624
shinusmp <shrinivaspatil12@gmail.com>
2022-06-15 14:15:17 +0530

    PHN-9 :Create serviceI Requred method

--- src/main/java/com/BikkadIt/phoneBook/serviceI/PhoneBookService.java
+++ src/main/java/com/BikkadIt/phoneBook/serviceI/PhoneBookService.java
@@ -1,5 +1,13 @@
 package com.BikkadIt.phoneBook.serviceI;
 
+import java.util.List;
+
+import com.BikkadIt.phoneBook.Model.Contact;
+
 public interface PhoneBookService {
+	
+	boolean saveContact(Contact contact);
+	List<Contact> viewContacts();
+	 Contact viewByid(Integer id);
 
 }
